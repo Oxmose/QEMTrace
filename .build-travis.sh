6f8c7ba6b38ebaf9c12f99999c1e3386846977f5
Oxmose <alexy.torresa@gmail.com>
2019-10-17 17:30:41 -0400

    Prepared repo for Travis

---
+++ .build-travis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+error=0
+cd Tests
+for d in */ ; do
+    cd $d
+    chmod +x *.sh
+    ./test_suite.sh
+    val=$?
+    if (( $val != 0 ))
+    then
+        echo -e "\e[31mERROR \e[39m"
+        error=$(($error0 + 1))
+    else
+        echo -e "\e[92mPASSED\e[39m"
+    fi
+done
+cd ..
+exit $error
