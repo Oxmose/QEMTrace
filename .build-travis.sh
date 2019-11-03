ecbf6787539b9903d442ac743a92d2b7911f1188
Oxmose <alexy.torresa@gmail.com>
2019-11-03 17:14:05 -0500

    Updated test script

--- .build-travis.sh
+++ .build-travis.sh
@@ -2,21 +2,21 @@
 
 error=0
 arm-none-eabi-gcc --version
-# cd Tests
-# for d in */ ; do
-#     echo -e "\e[92m In $d\e[39m"
-#     cd $d
-#     chmod +x *.sh
-#     ./test_suite.sh
-#     val=$?
-#     if (( $val != 0 ))
-#     then
-#         echo -e "\e[31mERROR \e[39m"
-#         error=$(($error0 + 1))
-#     else
-#         echo -e "\e[92mPASSED\e[39m"
-#     fi
-#     cd ..
-# done
-# cd ..
+cd Tests
+for d in */ ; do
+    echo -e "\e[92m In $d\e[39m"
+    cd $d
+    chmod +x *.sh
+    ./test_suite.sh
+    val=$?
+    if (( $val != 0 ))
+    then
+        echo -e "\e[31mERROR \e[39m"
+        error=$(($error0 + 1))
+    else
+        echo -e "\e[92mPASSED\e[39m"
+    fi
+    cd ..
+done
+cd ..
 exit $error
