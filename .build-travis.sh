f4195483ca860bf81a7ba0b054b77a9a67d19c7f
Oxmose <alexy.torresa@gmail.com>
2019-11-03 16:46:10 -0500

    Experiment with Travis compiler that has a different behaviour than my compiler

--- .build-travis.sh
+++ .build-travis.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 error=0
-cd Tests
-for d in */ ; do
-    echo -e "\e[92m In $d\e[39m"
-    cd $d
-    chmod +x *.sh
-    ./test_suite.sh
-    val=$?
-    if (( $val != 0 ))
-    then
-        echo -e "\e[31mERROR \e[39m"
-        error=$(($error0 + 1))
-    else
-        echo -e "\e[92mPASSED\e[39m"
-    fi
-    cd ..
-done
-cd ..
+arm-none-eabi-gcc --version
+# cd Tests
+# for d in */ ; do
+#     echo -e "\e[92m In $d\e[39m"
+#     cd $d
+#     chmod +x *.sh
+#     ./test_suite.sh
+#     val=$?
+#     if (( $val != 0 ))
+#     then
+#         echo -e "\e[31mERROR \e[39m"
+#         error=$(($error0 + 1))
+#     else
+#         echo -e "\e[92mPASSED\e[39m"
+#     fi
+#     cd ..
+# done
+# cd ..
 exit $error
